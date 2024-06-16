844830f4c58d0e918514a22d9c119e9f4bb77207
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2024-06-16 15:26:11 +0100

    added log to endpoint

--- src/main/java/com/zee/demok8s/testk8/TestK8sController.java
+++ src/main/java/com/zee/demok8s/testk8/TestK8sController.java
@@ -1,6 +1,7 @@
 package com.zee.demok8s.testk8;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author : Ezekiel Eromosei
  * @code @created : 15 Jun, 2024
  */
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "test-k8s")
@@ -22,6 +23,7 @@ public class TestK8sController {
 
     @GetMapping
     public ResponseEntity<K8sResponse> getData(@RequestParam(value = "mode")String mode){
+        log.info("test ck8 called -->>> {}", System.currentTimeMillis());
         return ResponseEntity.ok(
                 new K8sResponse(
                         Mode.getMode(mode),
